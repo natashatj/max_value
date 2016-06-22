9c75a0b60bf924bde5fea6472e93d10c700b2109
Natasha Jadavji <natasha@jadavji.ca>
2016-06-22 01:44:49 +0000

    inject

---
+++ max.rb
@@ -0,0 +1,17 @@
+# # Find the maximum 
+def maximum(arr)
+  arr.inject(0) {|max, num| num > max ? num : max }
+end
+
+result = maximum([2, 42, 22, 02])
+puts "max of 2, 42, 22, 02 is: #{result}"
+
+# expect it to return nil when empty array is passed in
+result = maximum([])
+puts "max on empty set is: #{result.inspect}"
+
+result = maximum([-23, 0, -3])
+puts "max of -23, 0, -3 is: #{result}"
+
+result = maximum([6])
+puts "max of just 6 is: #{result}"
